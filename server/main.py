695d9ec76e48923e0b9c7c7e037aece3ffa37458
frothsavd@gmail.com <frothsavd@gmail.com>
2021-06-30 14:35:34 -0400

    auto update

--- server/main.py
+++ server/main.py
@@ -6,6 +6,15 @@ async def main():
     ctx= await AuctionContext.create()
     server= Server(ctx)
 
+    async def auto_update():
+        while True:
+            print('auto updating')
+            await asyncio.sleep(ctx.CONFIG['auto_update_interval'])
+            await ctx.do_thread_update()
+
+    asyncio.create_task(auto_update())
+
+
 if __name__ == "__main__":
     loop= asyncio.get_event_loop()
     loop.create_task(main())
